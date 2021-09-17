55ee30d0cd7619983f6fae370b3c17f22b5689a3
BytesGalore <3903215+BytesGalore@users.noreply.github.com>
2021-09-17 16:46:20 +0200

    refactor: updated module registration to core rev 4++

---
+++ src/COL_loader.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ * Copyright (C) 2021+ WarheadCore <https://github.com/WarheadCore>
+ */
+
+void AddCongratsOnLevelScripts();
+
+void Addmod_congrats_on_levelScripts()
+{
+    AddCongratsOnLevelScripts();
+}
